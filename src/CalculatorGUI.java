2d27121b44849c80e67d4e9bfc694c19c3e7b945
지섭 김 <jskim6x@gmail.com>
2021-08-05 01:05:20 +0900

    20210805 - 1. 각 버튼 배열화(반복문으로 접근 용이) 2. 주석 설명 3. 클릭 리스너 함수생성 (각 버튼 리스너)

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -1,26 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CalculatorGUI extends JFrame {
-    CalculatorPanel panel;
-    JLabel label;
+    //라벨과 커스텀패널 객체생성
+    CalculatorPanel panel = new CalculatorPanel();
+    JLabel label = new JLabel("0");
+    //계산기 GUI클래스 생성자
     public CalculatorGUI(){
         initdata();
         layoutInitdata();
+        addClickListener();
     }
+    //프레임 관련 함수
     public void initdata(){
         this.setTitle("계산기");// 프레임 명을 정한다.
         this.setSize(400,600);// 프레임의 사이즈를 정한다
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);//크기 조절 불가능하게
-        panel = new CalculatorPanel();
-        label = new JLabel("0");
+    }
+    //레이아웃 설정 관련 함수
+    public void layoutInitdata(){
         label.setOpaque(true);
         label.setBackground(Color.WHITE);
-        //label.setForeground(Color.WHITE);
-    }
-    public void layoutInitdata(){
 
         panel.setPreferredSize(new Dimension(400,400));
 
@@ -31,6 +35,37 @@ public class CalculatorGUI extends JFrame {
 
         this.add(label,BorderLayout.NORTH);
         this.add(panel,BorderLayout.SOUTH);
+    }
+    //클릭에 관한 함수
+    public void addClickListener(){
+        //숫자버튼 클릭 시
+        for(int i=0;i<=9;i++){
+            panel.btn[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(label.getText().equals("0")){// 초기 화면을 0이라고 잡아놨고
+                        label.setText("");          // 다른숫자들을 타이핑했을 때 0이 사라지면서 숫자들이 나열되어야
+                    }                               // 상식적이라고 생각했다.
+                    String number = e.getActionCommand();
+                    label.setText(label.getText() + number);
+                }
+            });
+        }
+        //연산자 클릭 시
+        for(int i=14;i<=17;i++){
+            panel.btn[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(label.getText().equals("0")){
+                        label.setText("0");
+                    }else{
+                        String operator = e.getActionCommand();
+                        label.setText(label.getText() + operator);
+                    }
+
+                }
+            });
+        }
     }
     int plus(int a,int b){
         return a+b;
--- src/CalculatorPanel.java
+++ src/CalculatorPanel.java
@@ -2,33 +2,38 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CalculatorPanel extends JPanel {
-    JButton bt1 = new JButton("C");
-    JButton bt2 = new JButton("√");
-    JButton bt3 = new JButton("<-");
-    JButton bt4 = new JButton("/");
-    JButton bt5 = new JButton("1");
-    JButton bt6 = new JButton("2");
-    JButton bt7 = new JButton("3");
-    JButton bt8 = new JButton("*");
-    JButton bt9 = new JButton("4");
-    JButton bt10 = new JButton("5");
-    JButton bt11 = new JButton("6");
-    JButton bt12 = new JButton("-");
-    JButton bt13 = new JButton("7");
-    JButton bt14 = new JButton("8");
-    JButton bt15 = new JButton("9");
-    JButton bt16 = new JButton("+");
-    JButton bt17 = new JButton("+/-");
-    JButton bt18 = new JButton("0");
-    JButton bt19 = new JButton(".");
-    JButton bt20 = new JButton("=");
+    JButton[] btn = new JButton[20];
     public CalculatorPanel(){
+        initData();
+    }
+    public void initData(){
         this.setLayout(new GridLayout(5,4));
-        //this.setSize(400,400);
-        this.add(bt1);this.add(bt2);this.add(bt3);this.add(bt4);
-        this.add(bt5);this.add(bt6);this.add(bt7);this.add(bt8);
-        this.add(bt9);this.add(bt10);this.add(bt11);this.add(bt12);
-        this.add(bt13);this.add(bt14);this.add(bt15);this.add(bt16);
-        this.add(bt17);this.add(bt18);this.add(bt19);this.add(bt20);
+        btn[0] = new JButton("0");
+        btn[1] = new JButton("1");
+        btn[2] = new JButton("2");
+        btn[3] = new JButton("3");
+        btn[4] = new JButton("4");
+        btn[5] = new JButton("5");
+        btn[6] = new JButton("6");
+        btn[7] = new JButton("7");
+        btn[8] = new JButton("8");
+        btn[9] = new JButton("9");
+        btn[10] = new JButton("C");
+        btn[11] = new JButton("√");
+        btn[12] = new JButton("<-");
+        btn[13] = new JButton("=");
+        btn[14] = new JButton("+");
+        btn[15] = new JButton("-");
+        btn[16] = new JButton("x");
+        btn[17] = new JButton("/");
+        btn[18] = new JButton("+/-");
+        btn[19] = new JButton(".");
+
+        this.add(btn[10]);  this.add(btn[11]);  this.add(btn[12]);  this.add(btn[14]);
+        this.add(btn[1]);   this.add(btn[2]);   this.add(btn[3]);   this.add(btn[15]);
+        this.add(btn[4]);   this.add(btn[5]);   this.add(btn[6]);   this.add(btn[16]);
+        this.add(btn[7]);   this.add(btn[8]);   this.add(btn[9]);   this.add(btn[17]);
+        this.add(btn[18]);  this.add(btn[0]);   this.add(btn[19]);  this.add(btn[13]);
     }
 }
+
